7797a2fe6383cc750d04fef594880a8e85d827ba
AndriiPp <pyvovarov.andrii@gmail.com>
2019-01-20 00:05:18 +0200

    Initial commit

---
+++ RSSReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSReader
+//
+//  Created by Andrii Pyvovarov on 1/20/19.
+//  Copyright © 2019 Andrii Pyvovarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
